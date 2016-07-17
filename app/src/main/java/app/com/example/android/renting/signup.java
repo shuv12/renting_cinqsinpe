dd457d98becbdeb68aaae9dc973e70a5b79d6dd1
shuv12 <shuv2rockstar@gmail.com>
2016-07-17 12:27:51 +0545

    proper finish to signup/login page

--- app/src/main/java/app/com/example/android/renting/signup.java
+++ app/src/main/java/app/com/example/android/renting/signup.java
@@ -1,7 +1,10 @@
 package app.com.example.android.renting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class signup extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ImageView imageView = (ImageView) findViewById(R.id.backbutton);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
