8b0758a6ff83f8ed4b6240523d475dd23dca5e11
shuv12 <shuv2rockstar@gmail.com>
2016-07-24 13:01:54 +0545

    finish new post layout

--- app/src/main/java/app/com/example/android/renting/NewPost.java
+++ app/src/main/java/app/com/example/android/renting/NewPost.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 
 public class NewPost extends AppCompatActivity {
@@ -14,6 +17,35 @@ public class NewPost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+
+        HorizontalScrollView horizontalScrollView = (HorizontalScrollView) findViewById(R.id.hscrollv);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.translate_left_to_right);
+        horizontalScrollView.setAnimation(animation);
+
+
+       /* FrameLayout frameLayout = (FrameLayout) findViewById(R.id.map_frag);
+        frameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),GetLocationMapsActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
+       /* final LatLng baneshwor = new LatLng(27.280000,87.290000);
+        GoogleMap googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker marker = googleMap.addMarker(new MarkerOptions().position(baneshwor).draggable(true).title("SET Location"));
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Intent intent = new Intent(getApplicationContext(),GetLocationMapsActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
+
+
 
 
         ImageButton imageuploadbutton = (ImageButton) findViewById(R.id.add_home_image);
